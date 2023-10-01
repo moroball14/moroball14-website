16bff4f1ec25a66c840483aa2dec367ce889f840
moroball14 <daiki.mr.0330@gmail.com>
2023-10-01 16:19:28 +0900

    chore: change site description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,10 +20,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`${siteConfig.title}`} description="moroball14のwebサイト">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
